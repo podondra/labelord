f5989e98759be99efe552d8563482b83ba55fdce
Ondrej Podsztavek <ondrej.podsztavek@gmail.com>
2017-10-06 16:29:17 +0200

    Basic implementation of list_repos

--- labelord.py
+++ labelord.py
@@ -1,16 +1,40 @@
+import sys
 import click
 import requests
+import configparser
 
 
 @click.group('labelord')
+@click.option('-c', '--config', default='./config.cfg',
+        type=click.Path())
+@click.option('-t', '--token', envvar='GITHUB_TOKEN')
 @click.pass_context
-def cli(ctx):
+def cli(ctx, config, token):
     # TODO: add and process required app-wide options
     # you can/should use context 'ctx' for passing
     # data and objects to commands
 
-    # use this session for communication with github
+    # use this session for communication with GitHub
     session = ctx.obj.get('session', requests.Session())
+
+    ctx.obj['session'] = session
+
+    cfg = configparser.ConfigParser()
+    cfg.read(config)
+
+    if token is None:
+        try:
+            token = cfg['github']['token']
+        except KeyError:
+            click.echo('No GitHub token has been provided')
+            sys.exit(3)
+
+    def token_auth(req):
+        req.headers['Authorization'] = 'token ' + token
+        return req
+
+    session.headers = {'User-Agent': 'Python'}
+    session.auth = token_auth
 
 
 @cli.command()
@@ -18,7 +42,12 @@ def cli(ctx):
 def list_repos(ctx):
     # TODO: add required options/arguments
     # TODO: implement the 'list_repos' command
-    ...
+    session = ctx.obj['session']
+    payload = {'per_page': 100, 'page': 1}
+    r = session.get('https://api.github.com/user/repos',
+            params=payload)
+    for repo in r.json():
+        click.echo(repo['full_name'])
 
 
 @cli.command()

628b0048e43824fd10ec394f7d761058dfe965b2
Ondrej Podsztavek <ondrej.podsztavek@gmail.com>
2017-10-10 23:30:40 +0200

    Change setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@ from setuptools import setup
 
 setup(
     name='labelord',
-    version='0.1',
+    # TODO advance to 0.1 when finished
+    version='0.0',
     py_modules=['labelord'],
     install_requires=[
-        'click',
-        'requests',
+        'click>=6.7',
+        'requests>=2.18.4',
         ],
     entry_points='''
         [console_scripts]
         labelord=labelord:cli
         ''',
+    author='Ondřej Podsztavek',
+    author_email='ondrej.podsztavek@gmail.com',
     )

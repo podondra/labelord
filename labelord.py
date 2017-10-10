36d3ce1dacc3c4230360cec2505b441c7a567017
Ondrej Podsztavek <ondrej.podsztavek@gmail.com>
2017-10-10 19:41:37 +0200

    Pass test_list_repos.py

--- labelord.py
+++ labelord.py
@@ -1,40 +1,39 @@
+import re
 import sys
+import functools
 import click
 import requests
 import configparser
 
 
+def token_auth(req, token):
+    req.headers['Authorization'] = 'token ' + token
+    return req
+
+
 @click.group('labelord')
-@click.option('-c', '--config', default='./config.cfg',
-        type=click.Path())
+@click.option('-c', '--config', default='./config.cfg', type=click.Path())
 @click.option('-t', '--token', envvar='GITHUB_TOKEN')
 @click.pass_context
 def cli(ctx, config, token):
-    # TODO: add and process required app-wide options
-    # you can/should use context 'ctx' for passing
-    # data and objects to commands
-
     # use this session for communication with GitHub
     session = ctx.obj.get('session', requests.Session())
-
+    # save session in context
+    # if the session already exists reassign the same session
     ctx.obj['session'] = session
 
     cfg = configparser.ConfigParser()
+    # if config file does not exist 'cfg' will be empty
     cfg.read(config)
 
-    if token is None:
-        try:
-            token = cfg['github']['token']
-        except KeyError:
-            click.echo('No GitHub token has been provided')
-            sys.exit(3)
-
-    def token_auth(req):
-        req.headers['Authorization'] = 'token ' + token
-        return req
+    try:
+        the_token = token if token else cfg['github']['token']
+    except KeyError:
+        click.echo('No GitHub token has been provided')
+        sys.exit(3)
 
     session.headers = {'User-Agent': 'Python'}
-    session.auth = token_auth
+    session.auth = functools.partial(token_auth, token=the_token)
 
 
 @cli.command()
@@ -43,11 +42,36 @@ def list_repos(ctx):
     # TODO: add required options/arguments
     # TODO: implement the 'list_repos' command
     session = ctx.obj['session']
-    payload = {'per_page': 100, 'page': 1}
-    r = session.get('https://api.github.com/user/repos',
-            params=payload)
-    for repo in r.json():
-        click.echo(repo['full_name'])
+
+    # TODO url creation
+    url = 'https://api.github.com/user/repos?per_page=100&page=1'
+    while True:
+        r = session.get(url)
+
+        try:
+            r.raise_for_status()
+        except requests.exceptions.HTTPError:
+            # TODO separate function
+            click.echo('GitHub: ERROR {} - {}'.format(r.status_code,
+                r.json()['message']))
+            if r.status_code == 401:
+                sys.exit(4)
+            else:
+                sys.exit(10)
+
+        for repo in r.json():
+            click.echo(repo['full_name'])
+
+        # TODO control statements and url matching
+        link = r.headers.get('link', None)
+        if link is None:
+            break
+        else:
+            next_page = re.match('<(.*)>; rel="next"', link)
+            if next_page is None:
+                break
+            url = next_page.group(1)
+
 
 
 @cli.command()

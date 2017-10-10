5f15ed284214f73cb7d3e5f81a70bdd7545789e5
Ondrej Podsztavek <ondrej.podsztavek@gmail.com>
2017-10-10 23:50:17 +0200

    Add help messages

--- labelord.py
+++ labelord.py
@@ -12,10 +12,15 @@ def token_auth(req, token):
 
 
 @click.group('labelord')
-@click.option('-c', '--config', default='./config.cfg', type=click.Path())
-@click.option('-t', '--token', envvar='GITHUB_TOKEN')
+@click.option('-c', '--config', default='./config.cfg', type=click.Path(),
+        help='Configuration file in INI format.')
+@click.option('-t', '--token', envvar='GITHUB_TOKEN',
+        help='Access token for GitHub API.')
 @click.pass_context
 def cli(ctx, config, token):
+    # with 'setup.py' the ctx.obj might be None
+    ctx.obj = ctx.obj if ctx.obj else {}
+
     # use this session for communication with GitHub
     session = ctx.obj.get('session', requests.Session())
     # save session in context
@@ -36,7 +41,7 @@ def cli(ctx, config, token):
     session.auth = functools.partial(token_auth, token=the_token)
 
 
-@cli.command()
+@cli.command(help='List all accessible GitHub repositories.')
 @click.pass_context
 def list_repos(ctx):
     # TODO: add required options/arguments
@@ -44,6 +49,7 @@ def list_repos(ctx):
     session = ctx.obj['session']
 
     # TODO url creation
+    # https://developer.github.com/v3/repos/
     url = 'https://api.github.com/user/repos?per_page=100&page=1'
     while True:
         r = session.get(url)
@@ -74,17 +80,30 @@ def list_repos(ctx):
 
 
 
-@cli.command()
+@cli.command(help='''List all labels set for a repository.
+
+        REPOSLUG is URL-friendly version of repository name (user/repository).
+        ''')
+@click.argument('reposlug')
 @click.pass_context
-def list_labels(ctx):
+def list_labels(ctx, reposlug):
     # TODO: add required options/arguments
     # TODO: implement the 'list_labels' command
+    # https://developer.github.com/v3/issues/labels/
     ...
 
 
-@cli.command()
+# TODO enhance help
+@cli.command(help='Labels update. MODE can be \'update\' or \'replace\'.')
+@click.argument('mode', type=click.Choice(['update', 'replace']))
+@click.option('-d', '--dry-run', is_flag=True, default=False,
+        help='Print actions but do not apply them on GitHub.')
+@click.option('-v', '--verbose', is_flag=True, default=False,
+        help='Print actions to standart ouput.')
+@click.option('-q', '--quiet', is_flag=True, default=False,
+        help='Do not write anything to stdout or stderr.')
 @click.pass_context
-def run(ctx):
+def run(ctx, mode, dry_run, verbose, quiet):
     # TODO: add required options/arguments
     # TODO: implement the 'run' command
     ...
